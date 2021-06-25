f6b0d7b998527276c57577666620f9dd74c6622c
codeblogger-02 <mahendranimsatkar02@gmail.com>
2021-06-25 10:27:48 +0530

    fixed one bug

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 const taskContainer = document.querySelector(".task_container") 
  
+const globalStore = [];
+
 const generateNewCard=(taskData) => `<div class="col-md-6 col-lg-4 mt-3" id=${taskData}>
 <div class="card">
   <div class="card-header d-flex justify-content-end gap-2">
@@ -19,6 +21,20 @@ const generateNewCard=(taskData) => `<div class="col-md-6 col-lg-4 mt-3" id=${ta
 </div>
 </div>`;
 
+const loadInitialCardData = () =>{
+  //localstorage to get tasky card data
+   const getCardData = localStorage.getItem("tasky")
+  // convert to normal object
+    const {cards} = JSON.parse(getCardData);
+  //loop over those array of task object to create HTML card, inject it to DOM
+    cards.map((cardObject) => {
+      taskContainer.insertAdjacentHTML("beforeend", generateNewCard(cardObject));
+ // update our globalStore
+      globalStore.push(taskData);
+    })
+
+};
+
 const saveChanges = () => {
 const taskData ={
     id: `${Date.now()}`,
@@ -30,4 +46,8 @@ const taskData ={
 
   
   taskContainer.insertAdjacentHTML("beforeend", generateNewCard(taskData));
+
+  globalStore.push(taskData);
+   
+  localStorage.setItem("tasky", JSON.stringify({cards:globalStore}));
 };
